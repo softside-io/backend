a15f6ccba6e5cfd38beab4fa40f86d1d3d30577f
eliassh <e_sharbim1@hotmail.com>
2024-02-03 18:46:15 +0200

    SSA-55: modify swagger documentation option to exclude control name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { ClassSerializerInterceptor, ValidationPipe, VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory, Reflector } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
 import validationOptions from './utils/validation-options';
@@ -23,9 +23,13 @@ async function bootstrap() {
 	app.useGlobalPipes(new ValidationPipe(validationOptions));
 	app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-	const options = new DocumentBuilder().setTitle('API').setDescription('API docs').setVersion('1.0').addBearerAuth().build();
+	const config = new DocumentBuilder().setTitle('API').setDescription('API docs').setVersion('1.0').addBearerAuth().build();
 
-	const document = SwaggerModule.createDocument(app, options);
+	const options: SwaggerDocumentOptions = {
+		operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+	};
+
+	const document = SwaggerModule.createDocument(app, config, options);
 	SwaggerModule.setup('docs', app, document);
 
 	await app.listen(configService.getOrThrow('app.port', { infer: true }));
